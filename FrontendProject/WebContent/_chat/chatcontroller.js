d18f5d27c0c27c5d36d25f1f382e351f555e7636
Yashashree Takne <ystakne@gmail.com>
2017-01-20 19:32:12 +0530

    worked on CSS and look of pages

--- FrontendProject/WebContent/_chat/chatcontroller.js
+++ FrontendProject/WebContent/_chat/chatcontroller.js
@@ -1,7 +1,7 @@
 app.controller("ChatController", function($scope,$rootScope ,ChatService) {
     $scope.messages = [];
     $scope.message = "";
-    $scope.max = 140;
+    $scope.max = 300;
     
     $scope.addMessage = function() {
       ChatService.send($rootScope.currentUser.username + " : " + $scope.message);
